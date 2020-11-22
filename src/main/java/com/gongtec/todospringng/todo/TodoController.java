f770659b5be4c25c2d586fe831042f601e708fdd
Lei Gong <leifgong@gmail.com>
2020-11-22 10:36:13 -0500

    add put and delete operations

--- src/main/java/com/gongtec/todospringng/todo/TodoController.java
+++ src/main/java/com/gongtec/todospringng/todo/TodoController.java
@@ -2,8 +2,11 @@ package com.gongtec.todospringng.todo;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,15 @@ public class TodoController {
   @PostMapping
   public Todo addTodo(@RequestBody Todo todo) {
     return todoRepository.save(todo);
+  }
+
+  @PutMapping
+  public Todo updateTodo(@RequestBody Todo todo) {
+    return todoRepository.save(todo);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteTodoById(@PathVariable Long id) {
+    todoRepository.deleteById(id);
   }
 }
