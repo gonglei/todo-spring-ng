9e1fb25dd9cb636c37cdca9237c68a21b404d3fb
Lei Gong <leifgong@gmail.com>
2020-11-22 09:35:03 -0500

    add respository and controller

---
+++ src/main/java/com/gongtec/todospringng/todo/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.gongtec.todospringng.todo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends CrudRepository<Todo, Long> {}
