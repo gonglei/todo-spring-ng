375939d237499a85ef3f6547a40f04edbf70207d
Lei Gong <leifgong@gmail.com>
2020-11-22 09:19:08 -0500

    initial commit with todo model

---
+++ src/main/java/com/gongtec/todospringng/TodoSpringNgApplication.java
@@ -0,0 +1,13 @@
+package com.gongtec.todospringng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringNgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gongtec/todospringng/TodoSpringNgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongtec.todospringng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringNgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
