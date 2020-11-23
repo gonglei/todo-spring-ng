ee2f558b91588ff91490f08d7f86f77401a0e3ca
Lei Gong <leifgong@gmail.com>
2020-11-22 21:59:55 -0500

    remove unecessary imports

--- ui/src/app/todo-list/confirm-delete/confirm-delete.component.ts
+++ ui/src/app/todo-list/confirm-delete/confirm-delete.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core'
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { MAT_DIALOG_DATA } from '@angular/material/dialog'
 import { Todo } from 'src/app/models/todo.model'
 
 @Component({
